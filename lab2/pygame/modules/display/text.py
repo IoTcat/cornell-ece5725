587202280f74bf140dc537b1fcc7e6236fe53487
iotcat <i@iotcat.me>
2022-09-21 18:02:34 -0400

    fix text position

--- lab2/pygame/modules/display/text.py
+++ lab2/pygame/modules/display/text.py
@@ -8,14 +8,17 @@ class Text(Entity):
     text = ''
     color = 255,255,255
     size = 35
-    def __init__(self, text = 'default', color = (255,255,255), position = [200, 200], size = 35, speed = [0,0]):
+    position = [0,0]
+    def __init__(self, text = 'default', color = (255,255,255), position = [0,0], size = 35, speed = [0,0]):
         self.speed = np.array(speed)
         self.text = text
         self.color = color
         self.size = size
+        self.position = position
         self.refresh()
 
 
     def refresh(self):
         self.surface = pygame.font.SysFont('Corbel',self.size, True).render(self.text, True, self.color)
         self.rect = self.surface.get_rect()
+        self.rect.center = self.position
